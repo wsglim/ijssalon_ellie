bb01a0e159ec282a9aba022f14627323cb5ba1cb
Wouter Glimmerveen <wouterglimmerveen@live.nl>
2023-06-13 13:38:22 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,10 +1,27 @@
-prijzen={"aardbei":"3","vanille":"4","chocolade":"5"}
-aanbieding=prijzen["vanille"]=4*.8
-reclame_tekst="vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € "
-reclame_tekst3=(f"{reclame_tekst}{aanbieding}0").swapcase()
-reclame_tekst4=reclame_tekst3.split(" ")
-for el in reclame_tekst4:
-    if len(el)<5:
-        print(el.lower())
-    else:
-        print(el.upper())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen={"aardbei":3,
+             "vanille":4,
+             "chocolade":5
+             }
+    
+    aanbieding=prijzen["vanille"]*.8
+
+    reclame_tekst=f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+
+    reclame_tekst2=reclame_tekst[:63]
+
+    reclame_tekst3=reclame_tekst2.upper()
+
+    reclame_tekst4=reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el)<5:
+            print(el.lower())
+        else:
+            print(el.upper())
+
+
+decoreer("Aanbieding")
+print_aanbieding()
